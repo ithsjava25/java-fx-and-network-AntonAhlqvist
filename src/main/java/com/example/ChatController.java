9e44e8ca6e2df2e998f4cb166f354eda343f2af7
Anton Ahlqvist <anton.ahlqvist@iths.se>
2025-11-04 22:33:39 +0100

    feat: send messages to server

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -1,9 +1,16 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -16,6 +23,15 @@ public class ChatController {
     @FXML
     private TextField messageInput;
 
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final String serverAdress;
+    private final String topic = "mytopic";
+
+    public ChatController() {
+        Dotenv dotenv = Dotenv.load();
+        serverAdress = dotenv.get("HOST_NAME");
+    }
+
     @FXML
     private void initialize() {
 
@@ -24,5 +40,18 @@ public class ChatController {
     public void handleSendMessage(ActionEvent actionEvent) {
         String message = messageInput.getText();
         messageList.getItems().add("Du skrev: " + message);
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(serverAdress + "/" + topic))
+                    .POST(HttpRequest.BodyPublishers.ofString(message))
+            .header("Cache", "no")
+                    .build();
+
+            httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+
+        } catch (IOException | InterruptedException e) {
+            messageList.getItems().add("kunde inte skicka");
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module chatclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires io.github.cdimascio.dotenv.java;
 
     opens com.example to javafx.fxml;
     exports com.example;
