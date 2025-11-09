53c1124b1cbca1559fe943a009b91799aa7c911a
Anton Ahlqvist <anton.ahlqvist@iths.se>
2025-11-09 11:01:49 +0100

    refactor: update message container layout

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -3,13 +3,15 @@ package com.example;
 import io.github.cdimascio.dotenv.Dotenv;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class ChatController {
 
     @FXML
-    ListView<String> messageList;
+    VBox messageContainer;
+
     @FXML
     TextField messageInput;
 
@@ -24,21 +26,22 @@ public class ChatController {
 
     @FXML
     private void initialize() {
-        messageList.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 16;");
-        messageList.setItems(model.getMessages());
+        messageContainer.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 16;");
         model.receiveMessage();
     }
 
     @FXML
     void handleLocalMessage(ActionEvent actionEvent) {
         String message = messageInput.getText();
-        messageList.getItems().add("Du skrev: " + message);
+        Label label = new Label("Du skrev: " + message);
+        messageContainer.getChildren().add(0, label);
     }
 
     @FXML
     void handleSendMessage(ActionEvent actionEvent) {
         String message = messageInput.getText();
-        messageList.getItems().add("Du skrev: " + message);
+        Label label = new Label("Du skrev: " + message);
+        messageContainer.getChildren().add(label);
         model.sendMessage(message);
     }
 }
