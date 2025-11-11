8c9cb6ee01c137f8bbc6d8b0f301b359df1dc7cc
Anton Ahlqvist <anton.ahlqvist@iths.se>
2025-11-11 22:46:09 +0100

    feat: add local tetris block spawning

--- src/main/java/com/example/ChatetrisController.java
+++ src/main/java/com/example/ChatetrisController.java
@@ -3,23 +3,52 @@ package com.example;
 import io.github.cdimascio.dotenv.Dotenv;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class ChatetrisController {
 
-    @FXML private ImageView backgroundImage;
-    @FXML private ImageView frameImage;
+    @FXML
+    private ImageView backgroundImage;
+
+    @FXML
+
+    private ImageView frameImage;
 
     @FXML
     TextField messageInput;
 
     @FXML
-    Button sendButton;
+    private
+    Pane messageLayer;
+
+    private int messageCount = 0;
 
     @FXML
-    Button localButton;
+    void tbButton() {
+        messageCount++;
+
+        int TILE_SIZE = 64;
+
+        for (int i = 0; i < messageLayer.getChildren().size(); i++) {
+            var block = messageLayer.getChildren().get(i);
+            double oldY = block.getLayoutY();
+            double newY = oldY + TILE_SIZE;
+            block.setLayoutY(newY);
+        }
+
+        Label tb = new Label(String.valueOf(messageCount));
+
+        tb.setPrefSize(TILE_SIZE, TILE_SIZE);
+        tb.getStyleClass().add("tetrisblock");
+        tb.setLayoutX(64);
+        tb.setLayoutY(64);
+
+        messageLayer.getChildren().add(tb);
+    }
 
     private final String serverAddress;
     private final ChatetrisModel model;
@@ -39,6 +68,12 @@ public class ChatetrisController {
                 getClass().getResource("/Images/Dark_Brown_Wood_Frame.png").toExternalForm()
         ));
     }
+
+    @FXML
+    Button sendButton;
+
+    @FXML
+    Button localButton;
 
     @FXML
     void handleLocalMessage() {
